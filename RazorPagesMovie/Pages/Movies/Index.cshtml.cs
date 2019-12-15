cb1f04681c967b27811cb13962ad925dcd559294
schlechte <mohamed.magdy.amer@gmail.com>
2019-12-16 00:11:42 +0100

    Adding Search by movie name and movie genre.

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -19,11 +20,34 @@ namespace RazorPagesMovie
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; }
+        public IList<Movie> Movie { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public SelectList Geners { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
+
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            var movies = from m in _context.Movie
+                    select m;
+            if(!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(m => m.Title.Contains(SearchString));
+            }
+
+            if(!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(m => m.Genre == MovieGenre);
+            }
+            Geners = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
